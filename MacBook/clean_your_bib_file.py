aadfb72d2871241e8f58103a6951172b32657392
Chao <zhang.chao@pku.edu.cn>
2015-11-04 10:27:23 -0800

    clean latex bib

---
+++ MacBook/clean_your_bib_file.py
@@ -0,0 +1,20 @@
+import os, re
+#import argparse
+def clean(fileName):
+	remove_keys = ['abstract', 'doi', 'isbn', 'mendeley-groups', 'keywords', 'file', 'issn']
+	blind_keys = ['url']
+	bibf = open(fileName, 'r')
+	cleanf = open(fileName + '.tmp', 'w')
+	lines = bibf.readlines()
+	for line in lines:
+		if re.search('^'+'|'.join(blind_keys),line):
+			cleanf.write('%%%%' + line)
+		elif not re.search('^'+'|'.join(remove_keys),line):
+			#print line
+			cleanf.write(line)
+	cleanf.close()
+	os.remove(fileName)
+	os.rename(fileName + '.tmp', fileName)
+
+clean('refs.bib')
+
