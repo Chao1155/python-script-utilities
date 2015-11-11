75577d4e2d3371a033a1df7f43eb8ee8484680d9
Chao Zhang <yishuihan.zc@gmail.com>
2015-11-11 10:45:39 -0800

    new stats_collector

--- UCSB_knot/stats_collector.py
+++ UCSB_knot/stats_collector.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-
 tags_dict = {}
 
 def add_search_item(key, output = "", diction = tags_dict):
@@ -36,40 +35,18 @@ PARSEC = [
     'ferret', 
     'fluidanimate', 
     'freqmine', 
-    #'rtview',
+    'rtview',
     'streamcluster',
     'swaptions', 
-    #'vips', 
+    'vips', 
     'x264', 
 ]
 sizes=['4MB']#, '128MB','192MB']
 	#file_dir = "/Users/ShuiHan/Yue_Parsec_64MB/canneal"
 
-def read_one_block(file_dir, tags_list, tags_value_dict, 
-	block_index = 3, file_name = "stats.txt"):
-	try:
-		stats_file = open (file_dir +'/'+ file_name, 'r')
-	except:
-		print "cannot open " + file_name + " in " + file_dir
-		return;
-
-	line_list = stats_file.readlines()
-	
-	block_counter = 0		# only fetch the data in # th block.
-
-	for i in range(len(line_list)):
-		if ( 'Begin Simulation Statistic' in line_list[i]):
-			block_counter += 1
-		if block_counter == block_index:
-			for tag in tags_list:
-				if ( tag in line_list[i]):
-				#print line_list[i]
-				# get the number which is the second and put into the dict.
-					tags_value_dict[tag] = line_list[i].split()[1]
-
-	stats_file.close()
-
-def read_all_blocks(file_dir, tags_list, file_name = "stats.txt"):
+def read_all_blocks(file_dir, tags_list, bundle = 1, file_name = "stats.txt"):
+    """ works as the main block of analyze function.
+    """
     try:
         stats_file = open (file_dir +'/'+ file_name, 'r')
     except:
@@ -80,77 +57,35 @@ def read_all_blocks(file_dir, tags_list, file_name = "stats.txt"):
     output_row_list = []
     #print output_row_list
 
+    # bund the stats of 'bundle' periods together.
+    bundle_remain = bundle 
     for i in range(len(line_list)):
+    # for each line do the following check
         if ( 'Begin Simulation Statistic' in line_list[i]):
-            output_row_list.append (dict([(key, '0') for key in tags_list]))
-            output_row_list[block_counter]['block'] = str(block_counter)
-            block_counter += 1
+            if bundle_remain == bundle:
+                output_row_list.append (dict([(key, 0) for key in tags_list]))
+                output_row_list[block_counter]['block'] = str(block_counter)
+                block_counter += 1
+                
         for tag in tags_list:
             if ( tag in line_list[i]):
-        	    output_row_list[block_counter -1 ][tag] = line_list[i].split()[1]
+        	    output_row_list[block_counter -1 ][tag] += float(line_list[i].split()[1])
+        if ( 'End Simulation Statistic' in line_list[i]):
+            bundle_remain -=1
+            if bundle_remain == 0:
+                bundle_remain = bundle
+            
     stats_file.close()
     return output_row_list
 
-def calculate_block(data_dict_rows):
-    if data_dict_rows is None:
-        return
-    dict_keys = list(data_dict_rows[0].keys())
-    dict_rows = len(data_dict_rows)
-    #print dict_keys, dict_rows
-    saved_time = 0
-    for row in xrange(dict_rows):
-        # calculate l2 access and cpu inst per period
-        if row == 0:
-            data_dict_rows[row]['L2 access pp'] = '0'
-            data_dict_rows[row]['N_inst pp'] = '0'
-        else:
-            data_dict_rows[row]['L2 access pp'] = \
-            data_dict_rows[row]['system.l2.overall_accesses::total']
-            data_dict_rows[row]['N_inst pp'] = \
-            str ( int(data_dict_rows[row]['sim_insts']) -
-                    int(data_dict_rows[row - 1]['sim_insts'])
-                )
-
-def analyze_files_by_one_block(data_dir):
-	output_column_list = ['benchmark', 'size']
-	output_column_list.extend(tags_list)
-	# print output_column_list
-
-	output_column_dict = dict([(key, '0') for key in output_column_list])
-
-	try:
-		output_file = open ('result.txt', 'w')
-	except:
-		print "cannot open result.txt as our result."
-		return;
-
-	#print "\t".join(output_column_list)
-	title_line = "\t".join(output_column_list)
-	output_file.write(title_line+'\n')
-	for bench in PARSEC:
-		for size in sizes:
-			# The name here is very disappointing, try to improve it.
-			file_dir = data_dir+size+"/"+bench
-			#print file_dir
-			read_one_block(file_dir,tags_list, output_column_dict, 3)
-			output_column_dict['benchmark'] = bench
-			output_column_dict['size'] = size
-			# print "\t".join(output_column_dict.values())
-			content_line = ''
-			for key in output_column_list:
-				content_line += output_column_dict[key] + '\t'
-			#print content_line
-			output_file.write(content_line+'\n')
-
-	output_file.close()
-
 def analyze_files_by_all_blocks(data_dir):
-    output_column_list = ['benchmark', 'size', 'block', 'L2 access pp', 'N_inst pp']
+    output_column_list = ['benchmark', 'size', 'block']
     output_column_list.extend(tags_list)
     # print output_column_list
     
     try:
-    	output_file = open ('result-100us-W2.csv', 'w')
+    	output_file = open ('result-10us.csv', 'w')
+    	#output_file = open ('result.txt', 'w')
     except:
     	print "cannot open result.txt as our result."
     	return;
@@ -168,20 +103,21 @@ def analyze_files_by_all_blocks(data_dir):
 			# The name here is very disappointing, try to improve it.
             file_dir = data_dir+size+"/"+bench
 			#print file_dir
-            output_list = read_all_blocks(file_dir,tags_list)
+            output_list = read_all_blocks(file_dir,tags_list, 10)
             if output_list is None:
                 continue;
-            calculate_block(output_list)
             for i in xrange(len(output_list)):
                 output_list[i]['benchmark'] = bench
                 output_list[i]['size'] = size
                 content_line = ''
+                #print output_list[i]
                 for key in output_column_list:
-                    content_line += output_list[i][key] + ','
+                    content_line += str(output_list[i][key]) + ','
                 output_file.write(content_line+'\n')
     output_file.close()
 
 #analyze_files("/Users/ShuiHan/Yue_Parsec_")
-analyze_files_by_all_blocks("./Parsec_100us_")
+#analyze_files_by_all_blocks("./Parsec_100us_")
+analyze_files_by_all_blocks("./Parsec_1us_")
 
 
